4e04d3307a43620380e3933ee1fc8e571817d661
Avi Khandakar <avikhandakar@gmail.com>
2024-10-15 18:15:03 +0600

    bug fix

--- components/discover/VipSignal.tsx
+++ components/discover/VipSignal.tsx
@@ -58,7 +58,8 @@ const VipSignal = () => {
       style={{
         paddingHorizontal: 12,
         backgroundColor: bgColor,
-      }}>
+      }}
+    >
       <View
         style={{
           flexDirection: "row",
@@ -66,14 +67,16 @@ const VipSignal = () => {
           paddingVertical: 20,
           alignItems: "center",
           backgroundColor: "transparent",
-        }}>
+        }}
+      >
         <Text
           style={{
             fontSize: 24,
             fontWeight: "bold",
             color: isDark ? "#D2D2D2" : "black",
-          }}>
-          Upcoming Features
+          }}
+        >
+          New Features
           {/* Vip <Foundation name="crown" size={24} color="black" /> */}
         </Text>
       </View>
@@ -89,13 +92,15 @@ const VipSignal = () => {
                 }}
                 colors={item?.bgColor}
                 start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 0 }}>
+                end={{ x: 1, y: 0 }}
+              >
                 <View
                   style={{
                     flexDirection: "row",
                     alignItems: "center",
                     gap: 12,
-                  }}>
+                  }}
+                >
                   <View
                     style={{
                       width: 36,
@@ -104,7 +109,8 @@ const VipSignal = () => {
                       justifyContent: "center",
                       alignContent: "center",
                       backgroundColor: "#000000",
-                    }}>
+                    }}
+                  >
                     {item.icon}
                   </View>
                   <View
@@ -112,14 +118,16 @@ const VipSignal = () => {
                       flex: 1,
                       flexDirection: "row",
                       justifyContent: "space-between",
-                    }}>
+                    }}
+                  >
                     <View style={{ gap: 4 }}>
                       <Text
                         style={{
                           fontSize: 20,
                           color: isDark ? "#FFD700" : "#8B7500",
                           fontWeight: "bold",
-                        }}>
+                        }}
+                      >
                         {item.title}
                       </Text>
                       <Text
@@ -127,7 +135,8 @@ const VipSignal = () => {
                           fontSize: 10,
                           color: isDark ? "#FFFFFF" : "#757575",
                           fontWeight: "normal",
-                        }}>
+                        }}
+                      >
                         {item.subTitle}
                       </Text>
                     </View>
@@ -135,13 +144,15 @@ const VipSignal = () => {
                       style={{
                         alignContent: "center",
                         justifyContent: "center",
-                      }}>
+                      }}
+                    >
                       <Text
                         style={{
                           fontSize: 14,
                           color: isDark ? "#FFFFFF" : "#757575",
                           fontWeight: "normal",
-                        }}>
+                        }}
+                      >
                         Demo 🚀
                       </Text>
                       {/* <Entypo
